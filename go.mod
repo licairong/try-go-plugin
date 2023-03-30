ee85a5fc87bef11ada056fb38eb2e4fe8424c07d
licairong <licairong@idcos.com>
2023-03-30 11:48:44 +0800

    change plugin

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/hashicorp/go-hclog v1.0.0 // indirect
 	github.com/hashicorp/go-plugin v1.4.3
+	github.com/licairong/avago v0.1.4
 	google.golang.org/grpc v1.43.0
 	google.golang.org/protobuf v1.25.0
 )
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-        "log"
-        "os/exec"
 	"github.com/hashicorp/go-plugin"
 	"github.com/licairong/try-go-plugin/shared"
+        r "github.com/licairong/avago"
 )
 
+/*
 type PluginService struct{}
 
 func (p PluginService) Hello(name string) (string, error) {
@@ -17,12 +17,13 @@ func (p PluginService) Hello(name string) (string, error) {
 	}
 	return string(out), nil
 }
+*/
 
 func main() {
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: shared.Handshake,
 		Plugins: map[string]plugin.Plugin{
-			"PrintPlugin": &shared.GRPCHelloPlugin{Impl: &PluginService{}},
+			"PrintPlugin": &shared.GRPCHelloPlugin{Impl: &r.PluginService{}},
 		},
 		// A non-nil value here enables gRPC serving for this plugin...
 		GRPCServer: plugin.DefaultGRPCServer,
